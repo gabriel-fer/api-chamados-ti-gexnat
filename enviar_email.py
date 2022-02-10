0982a2a71eac53177e6bea32c368c3e5456f70f7
Gabriel Ferreira <86082889+gabriel-fer@users.noreply.github.com>
2022-02-10 08:33:58 -0300

    Add files via upload
    A partir dessa atualização é possíel enviar um email do chamado cadastrado para toda a equipe de técnicos ficar sabendo que o email foi cadastrado.

---
+++ enviar_email.py
@@ -0,0 +1,43 @@
+from ast import stmt
+import smtpd
+import smtplib
+import email.message
+from datetime import datetime
+from senha import senha
+
+def enviar_email(chamado):
+    data = datetime.now()
+    dataabertura = data.strftime('%d/%m/%Y %H:%M')
+    j = eval(chamado)
+    user = j["user"]
+    cliente = j["cliente"]
+    contato = j["contato"]
+    relato = j["relato"]
+
+
+    corpo =f"""
+    <p>Prezados,</p>
+    <p>Um novo chamado foi aberto:</p>
+    <p>Quem abriu:{user}</p>
+    <p>Para quem é o chamado:{cliente}</p>
+    <p>telefone para contato: {contato}</p>
+    <p>data de abertura do chamado:{dataabertura}</p>
+    <p>Relato: {relato}</p>
+
+    
+    """
+    
+    msg = email.message.Message()
+    msg['Subject'] = "Assunto"
+    msg['From'] = 'tigexnat@gmail.com'
+    msg['to'] = '23paulogabriel@gmail.com'
+    senha_email = senha
+    msg.add_header('Content-Type','text/html')
+    msg.set_payload(corpo)
+
+    s = smtplib.SMTP('smtp.gmail.com:587')
+    s.starttls()
+
+    s.login(msg['From'],senha_email)
+    s.sendmail(msg['From'],[msg['to']],msg.as_string().encode('utf-8'))
+    print("email enviado")
